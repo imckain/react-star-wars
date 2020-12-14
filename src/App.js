c900da82ba1189ecd4a15f8b5c05878e78052bac
Ian McKain <ian@Ians-iMac.local>
2020-12-13 23:00:43 -0500

    cards are rendering- doing styling

--- src/App.js
+++ src/App.js
@@ -11,28 +11,48 @@ function App() {
 
   const [appData, setAppData] = useState({
     starships: [],
-    name: ''
+    // name: []
   });
 
+  
+
+  
+  
+  
+  
   async function getAppData() {
     const starshipData = await getAllStarships();
-    console.log(starshipData);
+    // console.log(starshipData);
     setAppData({
-      ...starshipData,
-      name: starshipData.results.name
+      starships: starshipData.results,
+      // name: starshipData.name
     })
   };
-
+  
   useEffect(() => {
     getAppData();
   }, []);
+  
+  
+  const starships = [appData.starships]
+  // const name = [appData.name]
+
+  // console.log(name)
+  console.log(starships)
+  console.log(appData)
 
   return (
     <div className="App">
       <header className="App-header">
-        STAR WARS STARSHIPS
+        <i class="fab fa-old-republic"></i>
+        <div>STAR WARS STARSHIPS</div>
+        <i class="fab fa-galactic-republic"></i>
       </header>
-      <StarShipCard starships={appData.starships} name={appData.name}/>
+      <div className="App-Star-Ship-Cards">
+        {starships[0].map((starship, idx) => 
+          <StarShipCard key={idx} starships={starship} name={starship.name} />
+        )}
+      </div>
     </div>
   );
 }
--- src/components/Starships/StarShipCard.js
+++ src/components/Starships/StarShipCard.js
@@ -1,12 +1,10 @@
+import styles from './StarShipCard.module.css';
+
 export function StarShipCard(props) {
 
-    return (
-        <div>
-            {props.starships.map((starship, idx) =>
-                <div key={idx}>
-                    {props.name}
-                </div>
-            )}
+    return (   
+        <div className={styles.StarShipCard}>
+            {props.name}
         </div>
     )
 }
